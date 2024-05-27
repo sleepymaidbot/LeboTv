dde2f9a06bdcbc3104ca9e6d80532ac02506ac18
Mathieu <158531180+G0ldsand01@users.noreply.github.com>
2024-05-26 22:28:18 -0400

    games page

--- svelte/src/routes/games/+page.svelte
+++ svelte/src/routes/games/+page.svelte
@@ -1,45 +1,60 @@
 <script>
-  import BeamNG from '$lib/assets/beamNG.png';
-  import mc from '$lib/assets/minecraft.png';
-  import pz from '$lib/assets/pz.png';
-  import terraria from '$lib/assets/terraria.png';
-  import * as Card from "$lib/components/ui/card/index";
+	import BeamNG from '$lib/assets/beamNG.png';
+	import mc from '$lib/assets/minecraft.png';
+	import pz from '$lib/assets/pz.png';
+	import terraria from '$lib/assets/terraria.png';
+	import * as Card from '$lib/components/ui/card/index';
 
-  const games = [
-    {
+	const games = [
+		{
 			name: 'BeamNG',
 			url: 'https://svip.sytes.net/lidarr',
-			image: BeamNG
+			image: BeamNG,
+			Port: 'Port : 30814/default'
 		},
 		{
 			name: 'Project Zomboid',
 			url: 'https://svip.sytes.net/radarr',
-			image: pz
+			image: pz,
+			Port: 'Port : 16262/default'
 		},
 		{
 			name: 'Terraria',
 			url: 'https://svip.sytes.net/emby',
-			image: terraria
+			image: terraria,
+			Port: 'Port : 7777/default'
 		},
 		{
 			name: 'Minecraft',
 			url: 'http://svip.sytes.net:5055/login',
-			image: mc
+			image: mc,
+			Port: 'Port : 25565/default'
 		}
-  ];
+	];
 </script>
 
-<div class="pt-20">
-	<h1>Select your Game server you want to Join!</h1>
-	{#each games as game}
-		<Card.Root class="items-center w-[25%]">
-			<Card.Header class="flex items-center justify-between">
-				<Card.Title>{game.name}</Card.Title>
-				<img src={game.image} alt="logo" width="64" height="64" />
-			</Card.Header>
-			<Card.Content>
-				<p>Card Content</p>
-			</Card.Content>
-		</Card.Root>
-	{/each}
+<div class="container flex min-h-screen flex-col items-center justify-center">
+	<div class="pb-5 text-4xl font-bold">
+		<h1>Select your Game server you want to Join!</h1>
+	</div>
+	<div class="flex w-[75%]">
+		{#each games as game}
+			<div class="h-[100%] w-[100%] pl-5">
+				<Card.Root class="h-[15%] w-[100%] items-center ">
+					<Card.Header class="flex items-center justify-between">
+						<Card.Title>{game.name}</Card.Title>
+						<img src={game.image} alt="logo" width="64" height="64" class="h-[64px] w-[64px]" />
+					</Card.Header>
+					<Card.Content>
+						<div class="flex items-center justify-center">
+							<p>svip.sytes.net</p>
+						</div>
+						<div class="flex items-center justify-center">
+							<p>{game.Port}</p>
+						</div>
+					</Card.Content>
+				</Card.Root>
+			</div>
+		{/each}
+	</div>
 </div>
